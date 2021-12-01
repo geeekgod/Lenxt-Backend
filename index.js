e3dc5b23a83443b19ba185bb83f42eb7acc3139d
Rishabh Singh <ris8645208@gmail.com>
2021-12-01 17:13:16 +0530

    feat: add entry point and test server

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Models, Routes & Controllers
+// const User = require("./models/users");
+
+// const authRouter = require("./routes/authRoute");
+
+// Config Dot env to access env's
+dotenv.config();
+
+// Mongo DB uri
+// const dbURI = `mongodb+srv://${process.env.APP_DB_USERNAME}:${process.env.APP_DB_PASSWORD}@cluster0.yx8a6.mongodb.net/${process.env.APP_DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+
+const port = process.env.PORT || 4500;
+
+// App start
+// mongoose
+//   .connect(dbURI)
+//   .then((res) => {
+//     // listen for requests
+//     console.log("connected to db", res);
+//     app.listen(port, () => {
+//       console.log("\nServer is live on http://localhost:3000");
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// middlewares
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log("\nServer is live on http://localhost:4500");
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello From Lenxt Api" });
+});
+
+// auth Routes
+// app.use("/auth", authRouter);
