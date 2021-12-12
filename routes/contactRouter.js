d3e1b628a71b731581b9b0c410e8e644fca902f8
AditiThakre <aditi.thakre05@gmail.com>
2021-12-12 10:58:06 +0530

    added files in controller and routes

---
+++ routes/contactRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const contactController = require("../controllers/contactController");
+
+router.get("/", contactController.listContacts);
+
+module.exports = router;
